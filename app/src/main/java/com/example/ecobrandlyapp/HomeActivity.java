6895a472e3cc32e7ecd5d3a7a1c9b6c3b12c3ed8
juhee77 <bora2233@naver.com>
2021-08-25 02:15:16 +0900

    baseFirebase- make login,sign in function

---
+++ app/src/main/java/com/example/ecobrandlyapp/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.example.ecobrandlyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        Button btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //로그아웃
+                mFirebaseAuth.signOut();
+                Intent logoutIntent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(logoutIntent);
+                finish();
+            }
+        });
+
+        //탈퇴처리하는 구문 (파이어베이스의 회원 정보 삭제)
+        //mFirebaseAuth.getCurrentUser().delete();
+
+    }
+}
---
+++ app/src/main/java/com/example/ecobrandlyapp/UserAccount.java
@@ -0,0 +1,65 @@
+package com.example.ecobrandlyapp;
+
+
+/**
+ * 사용자 계정 정보 모델 클래스
+ */
+public class UserAccount {
+
+
+
+    private String idToken;  //Firebase Uid
+    private String Id;
+    private String Pwd;
+    private String PhoneNumber;
+    private int level = 0;
+
+
+    public UserAccount() { }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getPwd() {
+        return Pwd;
+    }
+
+    public void setPwd(String pwd) {
+        Pwd = pwd;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+
+
+
+
+
+
+}
