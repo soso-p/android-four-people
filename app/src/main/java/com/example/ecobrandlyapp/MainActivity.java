d807f697e9f045e23b8415f195903f534fa53489
juhee77 <bora2233@naver.com>
2021-08-24 11:47:07 +0900

    link

--- app/src/main/java/com/example/ecobrandlyapp/MainActivity.java
+++ app/src/main/java/com/example/ecobrandlyapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.ecobrandlyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//화면에 무엇을 보여줄지 결정하는 메서드
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent); //액치비티 이동 구문
+            }
+        });
     }
+
+
 }
