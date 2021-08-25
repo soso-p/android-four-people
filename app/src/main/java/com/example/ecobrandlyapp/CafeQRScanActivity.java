c9977ee9f97b9d19b848e1f34dc9f740aef15795
happyfox11 <hanstar0211@kyonggi.ac.kr>
2021-08-25 17:14:17 +0900

    QR scan Simple Test

---
+++ app/src/main/java/com/example/ecobrandlyapp/CafeQRScanActivity.java
@@ -0,0 +1,75 @@
+package com.example.ecobrandlyapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CafeQRScanActivity extends AppCompatActivity{
+
+    private TextView storeId, timestamp, phoneNumber;
+    private Button btn_back;
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cafeqrscan);
+
+        qrScan = new IntentIntegrator(this);
+        qrScan.setPrompt("박스 안에 QR 코드를 스캔하세요!");
+        qrScan.initiateScan();
+
+        storeId = (TextView) findViewById(R.id.storeId);
+        timestamp = (TextView) findViewById(R.id.timestamp);
+        phoneNumber = (TextView)  findViewById(R.id.phoneNumber);
+        btn_back = (Button) findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CafeQRScanActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Intent intent = new Intent(CafeQRScanActivity.this, HomeActivity.class);
+                startActivity(intent);
+                Toast.makeText(CafeQRScanActivity.this, "스캔취소!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(CafeQRScanActivity.this, "스캔완료!", Toast.LENGTH_SHORT).show();
+                try {
+                    JSONObject obj = new JSONObject(result.getContents ());
+                    storeId.setText(obj.getString("storeId"));
+                    timestamp.setText(obj.getString("timestamp"));
+                    phoneNumber.setText(obj.getString("phoneNumber"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+
+
+}
