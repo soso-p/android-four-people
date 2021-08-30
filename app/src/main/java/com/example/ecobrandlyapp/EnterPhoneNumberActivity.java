dceffe1084da7e170a197877ea86ca0f0b233387
js <dlwltn0350@naver.com>
2021-08-30 14:55:22 +0900

    Add activity_enter_phone_number.xml

---
+++ app/src/main/java/com/example/ecobrandlyapp/EnterPhoneNumberActivity.java
@@ -0,0 +1,15 @@
+package com.example.ecobrandlyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterPhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_phone_number);
+
+    }
+}
